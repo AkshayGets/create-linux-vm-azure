fe0f4d286b824f0cb99785b98544bc9efc5738f5
AkshayGets <40195161+AkshayGets@users.noreply.github.com>
2020-05-14 18:48:31 +0530

    Update provider-variables.tf

--- provider-variables.tf
+++ provider-variables.tf
@@ -6,20 +6,20 @@
 
 variable "azure-subscription-id" {
   type        = string
-  description = "Azure Subscription ID"
+  description = "9343cde0-a2b2-4112-9cb1-4c9f7f0d9ca6"
 }
 
 variable "azure-client-id" {
   type        = string
-  description = "Azure Client ID"
+  description = "39299945-a005-4aa8-a718-6b8300c110b9"
 }
 
 variable "azure-client-secret" {
   type        = string
-  description = "Azure Client Secret"
+  description = "f7df5253-fa4e-4654-8797-f89f19e62d18"
 }
 
 variable "azure-tenant-id" {
   type        = string
-  description = "Azure Tenant ID"
+  description = "eadda20a-7514-472b-9369-91a734bf6ad2"
 }
